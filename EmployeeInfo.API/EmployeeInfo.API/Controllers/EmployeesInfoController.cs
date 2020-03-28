f82dda141306946d3510a2c7fc878d8e50e4b18f
Saurabh Vijay Gupta <saurabhgupta@apttus.com>
2020-03-28 13:35:36 +0530

    Asp.Net-Core-Web-Api

---
+++ EmployeeInfo.API/EmployeeInfo.API/Controllers/EmployeesInfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeInfo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesInfoController : ControllerBase
+    {
+        public JsonResult GetEmployees()
+        {
+            return new JsonResult(new List<object>()
+               {
+                 new {employeeid = 01989, Name = "ohn Patrick"},
+                 new {employeeid = 01987, Name= "Michael"},
+                 new {employeeid = 01988, Name= "Akhil Mittal"}
+               });
+        }
+    }
+}
